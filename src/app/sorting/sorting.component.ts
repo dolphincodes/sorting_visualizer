302b091aeeb72909921379e34722d78b9e2b5375
dolphincodes <seban19021997@gmail.com>
2020-09-22 05:45:40 +0530

    chore: Implemented basic ui and logic for bubble sort

---
+++ src/app/sorting/sorting.component.ts
@@ -0,0 +1,73 @@
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+
+@Component({
+  selector: 'app-sorting',
+  templateUrl: './sorting.component.html',
+  styleUrls: ['./sorting.component.css']
+})
+export class SortingComponent implements OnInit, OnChanges {
+
+  num = [
+    {value: 50, color: 'dodgerblue'},
+    {value: 6, color: 'dodgerblue'},
+    {value: 3, color: 'dodgerblue'},
+    {value: 12, color: 'dodgerblue'},
+    {value: 1, color: 'dodgerblue'}
+  ];
+  @Input() sort = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.sort.currentValue) {
+      this.bubbleSort();
+    }
+  }
+
+  public bubbleSort(): void {
+    let timestamp = 0;
+    for (let ptr1 = 0; ptr1 < this.num.length - 1; ptr1++) {
+      for (let ptr2 = 0; ptr2 < this.num.length - 1 - ptr1; ptr2++) {
+        (async () => {
+          await this.time(ptr2, timestamp);
+        })();
+        timestamp++;
+      }
+    }
+  }
+
+  public numberSwap(idx: number, timestamp: number): void {
+    setTimeout(() => {
+      const temp = this.num[idx + 1];
+      this.num[idx + 1] = this.num[idx];
+      this.num[idx] = temp;
+    }, 600 * timestamp);
+  }
+
+  public changeColor(idx: number, color: string, timestamp: number): void {
+    setTimeout(() => {
+      this.num[idx].color = color;
+      this.num[idx + 1].color = color;
+    }, 600 * timestamp);
+  }
+
+  public time(idx: number, timestamp: number): void {
+
+    setTimeout(() => {
+      (async () => {
+        this.changeColor(idx, 'green', 1);
+        if (this.num[idx].value > this.num[idx + 1].value) {
+          await this.changeColor(idx, 'red', 2);
+          await this.numberSwap(idx, 3);
+          this.changeColor(idx, 'green', 4);
+        }
+        await this.changeColor(idx, 'dodgerblue', 5);
+      })();
+    }, 3000 * timestamp);
+  }
+
+}
