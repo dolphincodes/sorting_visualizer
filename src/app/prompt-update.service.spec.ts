4c9dc7f496ad4df2cf893863555cbc9b480ea0ae
Seban V. Mathew <seban19021997@gmail.com>
2020-09-27 04:16:37 +0530

    (feat): Improved ui

--- src/app/prompt-update.service.spec.ts
+++ src/app/prompt-update.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { PromptUpdateService } from './prompt-update.service';
+import {PromptUpdateService} from './prompt-update.service';
 
 describe('PromptUpdateService', () => {
   let service: PromptUpdateService;
