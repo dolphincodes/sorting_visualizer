e798cf04539e128363a91d3efec70219cc5d5d66
Seban V. Mathew <seban19021997@gmail.com>
2020-09-29 06:21:24 +0530

    Implemented basic test conditions

--- src/app/prompt-update.service.spec.ts
+++ src/app/prompt-update.service.spec.ts
@@ -1,12 +1,24 @@
 import {TestBed} from '@angular/core/testing';
 
 import {PromptUpdateService} from './prompt-update.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {SwUpdate, UpdateActivatedEvent, UpdateAvailableEvent} from '@angular/service-worker';
+import {Observable, Subject} from 'rxjs';
 
 describe('PromptUpdateService', () => {
   let service: PromptUpdateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {provide: SwUpdate, useClass: SwUpdateServerMock}
+      ]
+    });
     service = TestBed.inject(PromptUpdateService);
   });
 
@@ -14,3 +26,17 @@ describe('PromptUpdateService', () => {
     expect(service).toBeTruthy();
   });
 });
+
+export class SwUpdateServerMock {
+  public available: Observable<UpdateAvailableEvent> = new Subject();
+  public activated: Observable<UpdateActivatedEvent> = new Subject();
+  public isEnabled = false;
+
+  public checkForUpdate(): Promise<void> {
+    return new Promise((resolve) => resolve());
+  }
+
+  public activateUpdate(): Promise<void> {
+    return new Promise((resolve) => resolve());
+  }
+}
